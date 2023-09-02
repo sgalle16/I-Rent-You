fc1d5ebf806caba7f30c36e51533af9e69a4679f
sgalle16 <sgalle16@eafit.edu.co>
2023-09-02 15:50:55 -0500

    user from property

--- backend/src/property/admin.py
+++ backend/src/property/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from .models import Property, PropertyComment, PropertyRating
+from .models import Property, PropertyComment, PropertyRating, User
 
 admin.site.register(Property)
+admin.site.register(PropertyComment)
+admin.site.register(PropertyRating)
+admin.site.register(User)
--- backend/src/property/models.py
+++ backend/src/property/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
 
 class Property(models.Model):
     PROPERTY_TYPES = [
@@ -19,7 +25,11 @@ class Property(models.Model):
     availability = models.BooleanField(default=True)
     images = models.ImageField(
         upload_to='media/property/property_images/', blank=True, null=True)
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')
+    
+# The line `owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')` in
+# the `Property` model is creating a foreign key relationship between the `Property` model and the
+# `User` model.
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='properties')
     description = models.TextField(blank=True)
 
     published_date = models.DateTimeField(auto_now_add=True)
@@ -32,15 +42,14 @@ class Property(models.Model):
 # The Comment class represents a comment on a Property object, with fields for the content of the
 # comment and the time it was created.
 class PropertyComment(models.Model):
-    property = models.ForeignKey(
-        Property, on_delete=models.CASCADE, related_name='comments')
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     time_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        # return f"{self.user.username.content}"
-        return self.content
+        return self.user.username
+        #return self.content
 
 # The Rating class represents a rating given to a property, with a value ranging from 1 to 5.
 
@@ -57,7 +66,7 @@ class PropertyRating(models.Model):
     value = models.PositiveIntegerField(choices=RATING_CHOICES)
     property = models.ForeignKey(
         Property, on_delete=models.CASCADE, related_name='ratings')
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.value
+        return self.user.username
