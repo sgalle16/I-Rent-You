755fdddbf7cbf0b26f80c53b5e3d1690b9aa9537
sgalle16 <sgalle16@eafit.edu.co>
2023-10-18 21:31:29 -0500

    paginator list

--- backend/src/property/views.py
+++ backend/src/property/views.py
@@ -1,9 +1,11 @@
+from random import shuffle
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from .models import Property, PropertyFeature, PropertyComment, PropertyRating
 from .forms import PropertyForm, PropertyFeatureForm
 from django.urls import reverse, reverse_lazy
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def home(request):
@@ -23,8 +25,13 @@ class PropertySearchListView(ListView):
         property_type = self.request.GET.get('property_type')
 
         if query:
-            queryset = queryset.filter(
-                Q(title__icontains=query) | Q(location__icontains=query))
+            keywords = query.split()
+            # Búsqueda por palabras clave; título, ubicación y descripción
+            keyword_queries = Q()
+            for keyword in keywords:
+                keyword_queries |= Q(title__icontains=keyword) | Q(
+                    location__icontains=keyword) | Q(description__icontains=keyword)
+            queryset = queryset.filter(keyword_queries)
 
         # Si se proporciona un valor para 'property_type', filtrar los objetos de Property por ese valor
         if property_type:
@@ -43,6 +50,27 @@ class PropertyListView(ListView):
         queryset = Property.objects.all()
 
         return queryset
+
+    paginate_by = 9
+
+    def get_context_data(self, **kwargs):
+        property_list = list(Property.objects.all())
+        shuffle(property_list)
+        # Establece la cantidad de elementos por página
+        paginator = Paginator(property_list, self.paginate_by)
+        page = self.request.GET.get('page')
+
+        try:
+            properties = paginator.page(page)
+        except PageNotAnInteger:
+            properties = paginator.page(1)
+        except EmptyPage:
+            properties = paginator.page(paginator.num_pages)
+
+        context = {
+            'properties': properties
+        }
+        return context
 
 
 class PropertyDetailView(DetailView):
